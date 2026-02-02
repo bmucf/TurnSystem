d7120d812f4e9e2e6d374104f54e77ed19a45009
Brian Mosquera <149270404+bmucf@users.noreply.github.com>
2026-02-01 22:58:24 -0500

    added cell data

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -4,6 +4,13 @@ using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
+    public class CellData
+    {
+        public bool Passable;
+    }
+
+    private CellData[,] m_BoardData;
+    
     private Tilemap m_Tilemap;
 
     public int mapLength;
@@ -17,19 +24,24 @@ public class BoardManager : MonoBehaviour
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
 
+        m_BoardData = new CellData[mapLength, mapWidth];
+
         for (int x = 0; x < mapLength; x++)
         {
             for (int y = 0; y < mapWidth; y++)
             {
                 Tile tile;
+                m_BoardData[x, y] = new CellData();
 
                 if (x == 0 ||  y == 0 || x == mapLength -1 || y == mapWidth -1)
                 {
                     tile = wallTiles[Random.Range(0, wallTiles.Length)];
+                    m_BoardData[x, y].Passable = false;
                 }
                 else
                 {
                     tile = groundTiles[Random.Range(0, groundTiles.Length)];
+                    m_BoardData[x, y].Passable = true;
                 }
 
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
